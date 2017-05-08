a505017df463e6a13e69ace748882d929533707b
zhnnnnn <475816908@qq.com>
2017-05-08 23:03:54 +0800

    code clean

---
+++ bantang/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  bantang
+//
+//  Created by 张辉男 on 17/5/8.
+//  Copyright © 2017年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ bantang/TestViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestViewController.m
+//  bantang
+//
+//  Created by 张辉男 on 17/5/8.
+//  Copyright © 2017年 zhn. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
